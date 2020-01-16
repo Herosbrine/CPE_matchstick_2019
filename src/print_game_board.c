2dec11ea099f4a66c6be8c56166633a1e9f71cce
Herosbrine <gregoire.bezier@epitech.eu>
2020-01-16 18:45:03 +0100

    [ADD] | interaction , lines, and matches

--- src/print_game_board.c
+++ src/print_game_board.c
@@ -6,8 +6,43 @@
 */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include "printf/my.h"
+int interaction2()
+{
+    char * buffer = NULL;
+    int fd = 0;
+    int save = 0;
+
+    buffer = malloc(sizeof(char) * 100);
+    my_printf("Matches: ");
+    fd = read(0, buffer, 256);
+    buffer[fd-1] = '\0';
+    save = my_getnbr(buffer);
+    free(buffer);
+    return (save);
+}
+
+int interaction(int params)
+{
+    char * buffer = NULL;
+    int fd = 0;
+    int save = 0;
+
+    buffer = malloc(sizeof(char) * 100);
+    my_printf("\nYour turn:\nLine: ");
+    fd = read(0, buffer, 256);
+    buffer[fd-1] = '\0';
+    save = my_getnbr(buffer);
+    free(buffer);
+    if (save > params) {
+        my_printf("Error: you cannot remove ");
+        my_printf("more than %d matches per turn", params);
+        interaction(params);
+    }
+    return (save);
+}
 
 int *print_game_board(int size)
 {
@@ -65,8 +100,13 @@ int *print_game_board(int size)
 int main(int ac, char **argv)
 {
     int size = my_getnbr(argv[1]);
+    int params = my_getnbr(argv[2]);
+    int Matches = 0;
+    int Lines = 0;
 
     if (ac < 3 || ac > 3)
         return (84);
     print_game_board(size);
+    Lines = interaction(params);
+    Matches = interaction2();
 }
