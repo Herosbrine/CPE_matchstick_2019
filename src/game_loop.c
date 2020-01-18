9db7dc2db7de1892a23a6890658e4caf0aededf2
Herosbrine <gregoire.bezier@epitech.eu>
2020-01-18 13:32:29 +0100

    [ADD] | win condtion

--- src/game_loop.c
+++ src/game_loop.c
@@ -13,13 +13,18 @@
 int check_map(t_data *cordonnee)
 {
     int i = 0;
+    int x = 0;
 
-    while(i < cordonnee->size + 1) {
-        if (i > 1)
-            return (0);
+    while (x < cordonnee->size + 1) {
+        if (x == cordonnee->size)
+            return(-1);
+        if (cordonnee->tab[i] == 0)
+            x++;
+        if  (cordonnee->tab[i] != 0)
+            return(0);
         i++;
     }
-    return (-1);
+    return(0);
 }
 
 void manage_stick(t_data *cordonnee)
@@ -39,10 +44,16 @@ int game_loop(t_data *cordonnee)
 {
     fill(cordonnee);
     //cordonnee->tab = print_game_board(cordonnee);
-    my_printf("\nYour turn:");
-    while (check_map(cordonnee) == 0) {
+    while (1) {
+        if (check_map(cordonnee) == -1) {
+            my_printf("\nI lost... snif... but I'll get you next time!!");
+            exit(1);
+        }
+        my_printf("\nYour turn:");
         verify_error(cordonnee);
         manage_stick(cordonnee);
+        my_printf("Player removed %d match(es)"\
+            " from line %d\n", cordonnee->save_2, cordonnee->save);
         print_game_board(cordonnee);
     }
     return (0);
