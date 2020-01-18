1b5200394f20d0a32e59082865085485fbd2e57a
Herosbrine <gregoire.bezier@epitech.eu>
2020-01-18 22:31:43 +0100

    [ADD] | print_game_board coding style

--- src/print_game_board.c
+++ src/print_game_board.c
@@ -10,12 +10,36 @@
 #include <stdio.h>
 #include "printf/my.h"
 
+void print_game_space(int *j, int *x, int *i, t_data *cordonnee)
+{
+    while (*i < cordonnee->size) {
+        *x = cordonnee->size * 2 - 1;
+        my_printf("*");
+        while (cordonnee->space[*i] > *j) {
+            my_printf(" ");
+            (*j)++;
+            (*x)--;
+        }
+        *j = 0;
+        while (cordonnee->tab[*i] > *j) {
+            my_printf("|");
+            (*j)++;
+            (*x)--;
+        }
+        *j = 0;
+        for (int k = 0; k < *x; k++)
+            my_printf(" ");
+        my_printf("*");
+        my_printf("\n");
+        (*i)++;
+    }
+}
+
 int *print_game_board(t_data *cordonnee)
 {
     int j = 0;
     int i = 0;
     int x = 0;
-    int size2 = cordonnee->size;
 
     i = 0;
     while(i < cordonnee->size * 2 + 1) {
@@ -24,28 +48,7 @@ int *print_game_board(t_data *cordonnee)
     }
     i = 0;
     my_printf("\n");
-    while (i < cordonnee->size) {
-        x = cordonnee->size * 2 - 1;
-        my_printf("*");
-        while (cordonnee->space[i] > j) {
-            my_printf(" ");
-            j++;
-            x--;
-        }
-        j = 0;
-        while (cordonnee->tab[i] > j) {
-            my_printf("|");
-            j++;
-            x--;
-        }
-        j = 0;
-        for (int k = 0; k < x; k++)
-            my_printf(" ");
-        my_printf("*");
-        my_printf("\n");
-        i++;
-        size2--;
-    }
+    print_game_space(&j, &x, &i, cordonnee);
     i = 0;
     while (i < cordonnee->size * 2 + 1) {
         my_printf("*");
