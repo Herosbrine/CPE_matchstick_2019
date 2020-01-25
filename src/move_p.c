cf5e765c52a9923f337c5c6d54c4deefb0366223
Herosbrine <gregoire.bezier@epitech.eu>
2020-01-25 17:54:19 +0100

    [ADD] | 5000 error management

--- src/move_p.c
+++ src/move_p.c
@@ -12,10 +12,14 @@
 
 int print_move_p(t_data *cordonnee)
 {
+    int y = 0;
+
     while (manage_stick(cordonnee) == 1) {
         my_printf("Error: not enough matches on this line\n");
         verify_error(cordonnee);
-        return (0);
+        y++;
+        if (y == 5000)
+            exit (0);
     }
     if (check_map(cordonnee) != -1) {
         my_printf("Player removed %d match(es)"\
